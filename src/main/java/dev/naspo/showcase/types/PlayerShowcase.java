39801a70d3963b5f0003fcd16a3c3d33e4fbeaa1
Athanasios <a.topaltsis@gmail.com>
2026-02-23 14:53:44 -0500

    Created PlayerShowcase type and refactored DataManager

---
+++ src/main/java/dev/naspo/showcase/types/PlayerShowcase.java
@@ -0,0 +1,40 @@
+package dev.naspo.showcase.types;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+// A player showcase.
+public class PlayerShowcase {
+    private ItemStack[] items;
+    private HashMap<Integer, Long> slotCooldowns;
+    private final UUID ownerUUID;
+
+    public PlayerShowcase(UUID ownerUUID) {
+        this.ownerUUID = ownerUUID;
+        slotCooldowns = new HashMap<>();
+    }
+
+    // Getters and Setters
+
+    public ItemStack[] getItems() {
+        return items;
+    }
+
+    public void setItems(ItemStack[] items) {
+        this.items = items;
+    }
+
+    public HashMap<Integer, Long> getSlotCooldowns() {
+        return slotCooldowns;
+    }
+
+    public void setSlotCooldowns(HashMap<Integer, Long> slotCooldowns) {
+        this.slotCooldowns = slotCooldowns;
+    }
+
+    public UUID getOwnerUUID() {
+        return ownerUUID;
+    }
+}
