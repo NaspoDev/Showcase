934b0ff9ca24ac10df1a7f8dcf4b4d6e8e0be804
Athanasios <a.topaltsis@gmail.com>
2026-02-27 23:40:20 -0500

    updated PlayerJoinListener

--- src/main/java/dev/naspo/showcase/listeners/PlayerJoinListener.java
+++ src/main/java/dev/naspo/showcase/listeners/PlayerJoinListener.java
@@ -1,6 +1,7 @@
 package dev.naspo.showcase.listeners;
 
 import dev.naspo.showcase.datamanagement.DataManager;
+import dev.naspo.showcase.types.PlayerShowcase;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,8 +21,9 @@ public class PlayerJoinListener implements Listener {
         Player player = event.getPlayer();
 
         // If the player does not have a showcase, create one for them.
-        if (!dataManager.getPlayerShowcases().containsKey(player.getUniqueId().toString())) {
-            dataManager.getPlayerShowcases().put(player.getUniqueId().toString(), new ItemStack[0]);
+        if (!dataManager.getPlayerShowcases().containsKey(player.getUniqueId())) {
+            PlayerShowcase showcase = new PlayerShowcase(player.getUniqueId());
+            dataManager.getPlayerShowcases().put(player.getUniqueId(), showcase);
         }
     }
 }
